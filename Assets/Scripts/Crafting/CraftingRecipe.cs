a5fe6846065695786ba61d4aab1378d6fc434623
DAMTech1507 <Deanam1507@gmail.com>
2020-01-14 12:29:52 +1100

    Crafting Update (Still not working)

---
+++ Assets/Scripts/Crafting/CraftingRecipe.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using DapperDino.Items;
+
+[Serializable]
+public struct ItemAmount
+{
+	public InventoryItem Item;
+	[Range(1, 999)]
+	public int Amount;
+}
+
+[CreateAssetMenu]
+public class CraftingRecipe : ScriptableObject
+{
+	public List<ItemAmount> Materials;
+	public List<ItemAmount> Results;
+
+    public Inventory inventory;
+
+	public bool CanCraft(IItemContainer itemContainer)
+	{
+		return HasMaterials(itemContainer);
+	}
+
+	private bool HasMaterials(IItemContainer itemContainer)
+	{
+		foreach (ItemAmount itemAmount in Materials)
+		{
+			if (inventory.ItemContainer.GetTotalQuantity(itemAmount.Item) < itemAmount.Amount)
+			{
+				Debug.LogWarning("You don't have the required materials.");
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	public void Craft(IItemContainer itemContainer)
+	{
+		RemoveMaterials(itemContainer);
+		AddResults(itemContainer);
+	}
+
+	private void RemoveMaterials(IItemContainer itemContainer)
+	{
+		foreach (ItemAmount itemAmount in Materials)
+		{
+			for (int i = 0; i < itemAmount.Amount; i++)
+			{
+				itemContainer.RemoveItem(new ItemSlot(itemAmount.Item , 1));
+			}
+		}
+	}
+
+	private void AddResults(IItemContainer itemContainer)
+	{
+		foreach (ItemAmount itemAmount in Results)
+		{
+			for (int i = 0; i < itemAmount.Amount; i++)
+			{
+				itemContainer.AddItem(new ItemSlot(itemAmount.Item, 1));
+			}
+		}
+	}
+}
--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -8,7 +8,7 @@ namespace DapperDino.Items
         [SerializeField] private Rarity rarity = null;
         [SerializeField] [Min(0)] private int sellPrice = 1;
         [SerializeField] [Min(1)] private int maxStack = 99;
-
+        [SerializeField] [Min(0)] private int id = 0;
         public override string ColouredName
         {
             get
@@ -20,5 +20,6 @@ namespace DapperDino.Items
         public int SellPrice => sellPrice;
         public int MaxStack => maxStack;
         public Rarity Rarity => rarity;
+        public int ID => id;
     }
 }
